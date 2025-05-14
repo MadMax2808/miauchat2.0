0fc7e19baf11ae79e3cb38fbbf29980c8972eba2
Celeste Aguilar <111248716+MadMax2808@users.noreply.github.com>
2025-05-14 16:21:14 -0600

    User error

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./userinfo.css";
+import "./userInfo.css";
 import { useUserStore } from "../../../lib/userStore";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
